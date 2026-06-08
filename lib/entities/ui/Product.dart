badddcb33d4e8408c641a9e71fb69db769e5e714
Lilyhouser <houselily010@gmail.com>
2026-06-08 14:31:07 +0700

    Lab 5: Add ProductDetail page when press 1 product

--- lib/entities/ui/Product.dart
+++ lib/entities/ui/Product.dart
@@ -3,12 +3,45 @@ class Product {
   String name;
   String? image;
   double price;
+  String? description;
 
   static List<Product> products = [
-    Product(id: 1, name: "Teddy bear", image: "image.img", price: 120000),
-    Product(id: 2, name: "Computer", image: "image.img", price: 390000),
-    Product(id: 3, name: "Barbie Doll", image: "image.img", price: 90000),
-    Product(id: 4, name: "Mobile", image: "image.img", price: 980000),
+    Product(
+      id: 1,
+      name: "Teddy bear",
+      image:
+          "https://cdn.florista.ph/uploads/product/floristaph/MAR2026/3FtBrownTeddyBear-1773838514957.webp",
+      price: 120,
+      description:
+          "This is a teddy bear. It is very cute. It is very cute. It is very cute. It is very cute. It is very cute. It is very cute. It is very cute.",
+    ),
+    Product(
+      id: 2,
+      name: "Computer",
+      image:
+          "https://cdn.florista.ph/uploads/product/floristaph/MAR2026/3FtBrownTeddyBear-1773838514957.webp",
+      price: 30,
+      description:
+          "This is a computer. It is very cute. It is very cute. It is very cute. It is very cute. It is very cute. It is very cute. It is very cute.",
+    ),
+    Product(
+      id: 3,
+      name: "Barbie Doll",
+      image:
+          "https://cdn.florista.ph/uploads/product/floristaph/MAR2026/3FtBrownTeddyBear-1773838514957.webp",
+      price: 97,
+      description:
+          "This is a doll. It is very cute. It is very cute. It is very cute. It is very cute. It is very cute. It is very cute. It is very cute.",
+    ),
+    Product(
+      id: 4,
+      name: "Mobile",
+      image:
+          "https://cdn.florista.ph/uploads/product/floristaph/MAR2026/3FtBrownTeddyBear-1773838514957.webp",
+      price: 89,
+      description:
+          "This is a mobile. It is very cute. It is very cute. It is very cute. It is very cute. It is very cute. It is very cute. It is very cute.",
+    ),
   ];
 
   factory Product.fromJson(Map<String, dynamic> json) {
@@ -17,6 +50,7 @@ class Product {
       name: json["name"],
       image: json["image"],
       price: json["price"],
+      description: json["description"],
     );
   }
 
@@ -25,6 +59,7 @@ class Product {
     required this.name,
     required this.image,
     required this.price,
+    required this.description,
   });
 
   void addProduct({
@@ -32,8 +67,17 @@ class Product {
     required String name,
     String? image,
     required double price,
+    String? description,
   }) {
-    products.add(Product(id: id, name: name, image: image, price: price));
+    products.add(
+      Product(
+        id: id,
+        name: name,
+        image: image,
+        price: price,
+        description: description,
+      ),
+    );
   }
 
   void editProduct({String? name, String? image, double? price}) {
@@ -77,6 +121,7 @@ class Product {
             name: e.name,
             price: (e.price * 1.1).roundToDouble(),
             image: e.image,
+            description: description,
           )),
         )
         .toList();
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:prm393_lab/entities/ui/BottomNavigation.dart';
 import 'package:prm393_lab/pages/home_page.dart';
+import 'package:prm393_lab/pages/product_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: BottomNavigation(),
       title: "Lily Lab 4",
     );
   }
